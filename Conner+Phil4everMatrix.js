6908de543d32c3d4a57e1f1b67e83781cd28ceae
sphsconormurphy <8cmurphy@stpatrick.org>
2017-01-23 13:57:20 -0600

    Maybe Done?

--- Conner+Phil4everMatrix.js
+++ Conner+Phil4everMatrix.js
@@ -7,19 +7,40 @@ let A = Matrix([
     [1,0,0,0]
 ]);
 
-function power(mtrx,p){
+//function power(mtrx,p){
     //mtrx is the matrix you input
     //p is the power of the matrix you want
-    let temp = mtrx;
-    for( let i = 1; i<p; i++){
-      temp = Matrix(temp.prod(mtrx))
+    //let temp = mtrx;
+    //for( let i = 0; i<p; i++){
+      //temp = Matrix(temp.prod(mtrx))
 
+//    }
 
-    }
-
-    return temp();
-}
+  //  return temp();
+//}
 
 
 //testing code
-console.log(power(A,10));
+//console.log(power(A,1));
+
+
+function power2(mtrx,p){
+
+  let  temp = mtrx;
+
+  for( let i = 0; i<p; i++){
+    if(p ==1){
+      return mtrx;
+    }
+    else if(p%2 == 0){
+      let half = power2(mtrx,p/2)
+      return half.prod(half);
+
+    }
+    else if(p%2 !== 0)
+      let half = power2(mtrx,math.ceil(p/2));
+      return half.prod(power2(p-half));
+  }
+}
+
+console.log(power2(A,60))
